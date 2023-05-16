0051f9e9c86f522b56dc90f2261957a1ff452caa
Alberto <alberto.savarese@fides.it>
2023-05-16 18:09:02 +0200

    [IMPROVEMENT] catchError warmup

--- src/functions/warmup/warmup.ts
+++ src/functions/warmup/warmup.ts
@@ -1,7 +1,7 @@
 import { EntityType } from "@libs/const";
 import { buildEntityPK } from "@libs/utils";
 import { APIGatewayProxyHandler } from "aws-lambda";
-import { from, lastValueFrom, mergeMap, range, toArray } from "rxjs";
+import { catchError, from, lastValueFrom, mergeMap, of, range, tap, toArray } from "rxjs";
 import { ProjectEntity } from "src/entity/project.entity";
 import { IProject } from "src/model/project.model";
 import axios from "axios";
@@ -18,9 +18,12 @@ export const handler: APIGatewayProxyHandler = async (_event, _context) => {
             return range(0, project.concurrency)
                 .pipe(mergeMap(_occurrency => {
                     return from(axios.get(project.url))
-                            // .pipe(tap(_e => {
-                            //     console.log(`${project.url} := ${_occurrency}`)
-                            // }))
+                            .pipe(tap(_e => {
+                                console.log(`${project.url} := ${_occurrency}`)
+                            }),catchError((e) => {
+                                console.log(e);
+                                return of(false)
+                            }))
                 }), toArray())
         }),
         toArray()))
